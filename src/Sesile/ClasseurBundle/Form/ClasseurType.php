471a1898961476996945010b2112df2cb8ffa567
Sandrive Vergon <s.vergon@sictiam.fr>
2013-12-04 17:27:23 +0100

    Petit boulot de sandrine avec regis

---
+++ src/Sesile/ClasseurBundle/Form/ClasseurType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Sesile\ClasseurBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ClasseurType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('description')
+            ->add('circuit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sesile\ClasseurBundle\Entity\Classeur'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sesile_classeurbundle_classeur';
+    }
+}
