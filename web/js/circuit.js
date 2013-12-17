5491766207e013e208c8128b480321b127351cbe
r.lasry <r.lasry@sictiam.fr>
2013-12-17 15:25:21 +0100

    Création Circuits OK Ajout des circutis enregistrés

--- web/js/circuit.js
+++ web/js/circuit.js
@@ -1,119 +1,111 @@
-(function($){
-    var Circuiter = function(element, options)
-    {
-        var elem = $(element);
-        var obj = this;
-        var settings = $.extend({
-
-        }, options || {});
-    };
-
-    $.fn.circuiter = function(options) {
-        return this.each(function()
-        {
-            var defaults = {
-                image_src: "/images/bonhomme.jpg",
-                users: new Array()
-            };
-
-            var params = $.extend(defaults, options);
-
-            // TODO l'élement choisi doit être un select
-            that = $(this);
+perso_src = "/images/imghomme.png";
+favoris_src = "/images/bookmark.png";
+fleche_src = "/images/flechecircuit.png";
+delete_src = "/images/delete.png";
+favoris_url = "";
+del_favoris_url = "";
 
 
-            // cadre pour afficher les personnes du circuit
-            circuit = $("<div/>").attr("id", "circuit").insertAfter(that);
-            that.data("circuit", circuit);
-            // bouton OK
-            bouton = $("<button/>").attr({id: "user_ok", type: "button"}).text("OK").insertAfter(that);
+$('#users_list option:first').attr("selected", "selected");
 
 
+/*
+ Ajoute un utilisateur dans le cadre "circuits"
+ */
+function ajoutUser(id) {
+    var sel_user = $('#users_list option[value="' + id + '"]');
+    if(sel_user.length == 0) {
+        return false;
+    }
+    var new_perso = $('<div/>').data('id', id).addClass('perso_circuit').appendTo("#circuit");
+    $('<img />').attr("src", perso_src).appendTo(new_perso);
+    $('<span class="nom_perso" />').text(sel_user.text()).appendTo(new_perso);
 
-            // si des users ont été spécifiés on les ajoute en personnages :)
-            if(params.users.length > 0) {
-                $.each(params.users, function(k, v) {
-                    var user_to_ajout = that.find('option[value="' + v + '"]');
+    sel_user.remove();
 
-                    var new_perso = $('<div/>').data('id', user_to_ajout.val()).addClass('perso_circuit').appendTo(circuit);
-                    $('<img />').attr("src", params.image_src).appendTo(new_perso);
-                    $('<span class="nom_perso" />').text(user_to_ajout.text()).appendTo(new_perso);
+    if ($('#users_list option').length == 0) {
+        $("#useradd_btn").prop('disabled', true);
+    }
 
-                    user_to_ajout.remove();
-                    // desactive le bouton si select si vide
-                    if(that.find('option').length == 0) {
-                        bouton.prop('disabled', true);
-                    }
+    if ($(".perso_circuit").length > 1) {
+        creerFleches();
+    }
+}
 
-                    // ajout de la fleche
-                    if(circuit.find(".perso_circuit").length > 1) {
-                        $('<div/>').html("&larr;").addClass('fleche_circuit').html('&rarr;').insertBefore(circuit.find(".perso_circuit:last"));
-                    }
-                });
-            }
+function creerFleches () {
+    $(".fleche_circuit").remove();
+    $("<img/>").attr("src", fleche_src).addClass("fleche_circuit").insertAfter(".perso_circuit:not(:last)");
+}
 
-            bouton.click(function() {
-                var sel_user = that.find(":selected");
-                var new_perso = $('<div/>').data('id', sel_user.val()).addClass('perso_circuit').appendTo(circuit);
-                $('<img />').attr("src", params.image_src).appendTo(new_perso);
-                $('<span class="nom_perso" />').text(sel_user.text()).appendTo(new_perso);
-
-                sel_user.remove();
-                // desactive le bouton si select si vide
-                if(that.find('option').length == 0) {
-                    $(this).prop('disabled', true);
-                }
-
-                // ajout de la fleche
-                if(circuit.find(".perso_circuit").length > 1) {
-                    $('<div/>').html("&larr;").addClass('fleche_circuit').html('&rarr;').insertBefore(circuit.find(".perso_circuit:last"));
-                }
-            });
-
-            circuit.sortable({
-                items: '.perso_circuit',
-                placeholder: 'emplacement',
-                update: function(event, ui) {
-                    $('.fleche_circuit').remove();
-                    if($("#circuit .perso_circuit").length > 1) {
-                        $('<div/>').html("&larr;").addClass('fleche_circuit').html('&rarr;').insertAfter(circuit.find(".perso_circuit:not(:last)"));
-                    }
-                },
-                over: function(e, ui) { sortableIn = 1; },
-                out: function(e, ui) { sortableIn = 0; },
-                beforeStop: function (event, ui) {
-                    if (sortableIn == 0) {
-                        elem = ui.item;
-                        $("<option/>").val(elem.data("id")).text(elem.find(".nom_perso").text()).appendTo(that);
-                        elem.remove();
-                        bouton.prop('disabled', false);
-                        $('.fleche_circuit').remove();
-                        if($("#circuit .perso_circuit").length > 1) {
-                            $('<div/>').html("&larr;").addClass('fleche_circuit').html('&rarr;').insertAfter(circuit.find(".perso_circuit:not(:last)"));
-                        }
-                    }
-                }
-            }).disableSelection();
-
-            if (that.data('circuiter')) return;
-            var circuiter = new Circuiter(this, params);
-            that.data('circuiter', circuiter);
-
-
-            // si on est dans un form on lui passe le circuit qd on walid
-            var form_parent = that.parents("form")
-            if(form_parent.length ) {
-                form_parent.submit(function(e) {
-                    e.preventDefault();
-                    var ordre_circuit = new Array();
-                    $("#circuit .perso_circuit").each(function(k,v) {
-                        ordre_circuit.push($(this).data("id"));
-                    });
-                    $('<input />').attr('type', 'hidden').attr('name', "circuit").attr('value', ordre_circuit).appendTo(form_parent);
-                    this.submit();
-                    return false;
-                });
-            }
+function recreerCircuit (circuitArray) {
+    // si des users ont été spécifiés on les ajoute en personnages :)
+    if(circuitArray.length > 0) {
+        $.each(circuitArray, function(k, v) {
+            var sel_user = $('#users_list option[value="' + v + '"]');
+            ajoutUser(sel_user.val());
         });
-    };
-})(jQuery);
+        creerFleches();
+    }
+}
+
+
+$("#useradd_btn").click(function() {
+    var sel_user = $('#users_list option:selected');
+    if(sel_user.length > 0) {
+        ajoutUser(sel_user.val());
+    }
+    $('#users_list option:first').attr("selected", "selected");
+});
+
+$("#circuit").sortable({
+    items: '.perso_circuit',
+    placeholder: 'emplacement',
+    update: function(event, ui) {
+        creerFleches();
+    },
+    over: function(e, ui) { sortableIn = 1; },
+    out: function(e, ui) { sortableIn = 0; },
+    beforeStop: function (event, ui) {
+        if (sortableIn == 0) {
+            elem = ui.item;
+            $("<option/>").val(elem.data("id")).text(elem.find(".nom_perso").text()).appendTo("#users_list");
+            elem.remove();
+            $('#users_list option:first').attr("selected", "selected");
+            $("#useradd_btn").prop('disabled', false);
+            creerFleches();
+        }
+    }
+ }).disableSelection();
+
+
+$("#circuitadd_btn").click(function() {
+    var sel_circuit = $('#circuits_list option:selected');
+    var ordre = sel_circuit.attr("data-ordre").split(",");
+    if(ordre.length > 0) {
+        $("#circuit").empty();
+        $.each(ordre, function (k, v) {
+            ajoutUser(v);
+        });
+    }
+    $('#users_list option:first').attr("selected", "selected");
+});
+
+
+ // si on est dans un form on lui passe le circuit qd on walid
+var form_parent = $("#circuit").parents("form");
+
+if(form_parent.length > 0 ) {
+     form_parent.submit(function(e) {
+         e.preventDefault();
+         var ordre_circuit = new Array();
+         $("#circuit .perso_circuit").each(function(k,v) {
+            ordre_circuit.push($(this).data("id"));
+         });
+         $('<input />')
+             .attr({ 'type': 'hidden', 'name': "circuit" })
+             .val(ordre_circuit)
+             .appendTo(form_parent);
+         this.submit();
+         return false;
+     });
+}
