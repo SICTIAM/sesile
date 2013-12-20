6f505bdb1ea02343885647ca9d47d1cef73515c5
r.lasry <r.lasry@sictiam.fr>
2013-12-20 15:45:46 +0100

    enregistrement des classeurs ajout des statuts, du validant

--- src/Sesile/UserBundle/Entity/User.php
+++ src/Sesile/UserBundle/Entity/User.php
@@ -12,6 +12,7 @@ use FOS\UserBundle\Entity\User as BaseUser;
  * @ORM\Table()
  * @ORM\Entity(repositoryClass="Sesile\UserBundle\Entity\UserRepository")
  * @ORM\HasLifecycleCallbacks()
+ *
  */
 class User extends BaseUser
 {
@@ -26,8 +27,98 @@ class User extends BaseUser
         return $this->expiresAt;
     }
 
-
     public function getCredentialsExpireAt() {
         return $this->getCredentialsExpireAt();
+    }
+
+    /**
+     * @ORM\OneToMany(targetEntity="Sesile\ClasseurBundle\Entity\Classeur", mappedBy="user")
+     */
+    protected $classeurs;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Sesile\ClasseurBundle\Entity\Classeur", mappedBy="validant")
+     */
+    protected $classeurs_a_valider;
+
+    public function __construct() {
+        $this->classeurs = new ArrayCollection();
+        parent::__construct();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Add classeurs
+     *
+     * @param \Sesile\ClasseurBundle\Entity\Classeur $classeurs
+     * @return User
+     */
+    public function addClasseur(\Sesile\ClasseurBundle\Entity\Classeur $classeurs)
+    {
+        $this->classeurs[] = $classeurs;
+    
+        return $this;
+    }
+
+    /**
+     * Remove classeurs
+     *
+     * @param \Sesile\ClasseurBundle\Entity\Classeur $classeurs
+     */
+    public function removeClasseur(\Sesile\ClasseurBundle\Entity\Classeur $classeurs)
+    {
+        $this->classeurs->removeElement($classeurs);
+    }
+
+    /**
+     * Get classeurs
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getClasseurs()
+    {
+        return $this->classeurs;
+    }
+
+    /**
+     * Add classeurs_a_valider
+     *
+     * @param \Sesile\ClasseurBundle\Entity\Classeur $classeursAValider
+     * @return User
+     */
+    public function addClasseursAValider(\Sesile\ClasseurBundle\Entity\Classeur $classeursAValider)
+    {
+        $this->classeurs_a_valider[] = $classeursAValider;
+    
+        return $this;
+    }
+
+    /**
+     * Remove classeurs_a_valider
+     *
+     * @param \Sesile\ClasseurBundle\Entity\Classeur $classeursAValider
+     */
+    public function removeClasseursAValider(\Sesile\ClasseurBundle\Entity\Classeur $classeursAValider)
+    {
+        $this->classeurs_a_valider->removeElement($classeursAValider);
+    }
+
+    /**
+     * Get classeurs_a_valider
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getClasseursAValider()
+    {
+        return $this->classeurs_a_valider;
     }
 }
