da32867211bf1934c34563aed23967c1d01d69dd
Manon Humbert <m.humbert@sictiam.fr>
2013-12-20 15:18:08 +0100

    Pouet

---
+++ src/Sesile/DocumentBundle/Form/Type/SesileDocHistoryAdmin/FiltersType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sesile\DocumentBundle\Form\Type\SesileDocHistoryAdmin;
+
+use Admingenerated\SesileDocumentBundle\Form\BaseSesileDocHistoryAdminType\FiltersType as BaseFiltersType;
+
+/**
+ * FiltersType
+ */
+class FiltersType extends BaseFiltersType
+{
+}
