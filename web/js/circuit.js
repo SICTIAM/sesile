b721d822f68d61088afb46181881b37a06c00ed9
r.lasry <r.lasry@sictiam.fr>
2014-01-14 09:02:46 +0100

    repository classeur

--- web/js/circuit.js
+++ web/js/circuit.js
@@ -14,7 +14,7 @@ $('#users_list p:first, #circuits_list p:first').addClass("list_selected_element
  */
 function ajoutUser(id) {
     var sel_user = $('#users_list p[data-id="' + id + '"]');
-    if(sel_user.length == 0) {
+    if (sel_user.length == 0) {
         return false;
     }
     var new_perso = $('<div/>').data('id', id).addClass('perso_circuit').appendTo("#circuit");
@@ -29,15 +29,15 @@ function ajoutUser(id) {
     }
 }
 
-function creerFleches () {
+function creerFleches() {
     $(".fleche_circuit").remove();
     $("<span/>").addClass("fleche_circuit glyphicon glyphicon-arrow-right").insertAfter(".perso_circuit:not(:last)");
 }
 
-function recreerCircuit (circuitArray) {
+function recreerCircuit(circuitArray) {
     // si des users ont été spécifiés on les ajoute en personnages :)
-    if(circuitArray.length > 0) {
-        $.each(circuitArray, function(k, v) {
+    if (circuitArray.length > 0) {
+        $.each(circuitArray, function (k, v) {
             var sel_user = $('#users_list p[data-id="' + v + '"]');
             ajoutUser(v);
         });
@@ -46,9 +46,9 @@ function recreerCircuit (circuitArray) {
 }
 
 
-$("#useradd_btn").click(function() {
+$("#useradd_btn").click(function () {
     var sel_user = $('#users_list .list_selected_element');
-    if(sel_user.length > 0) {
+    if (sel_user.length > 0) {
         ajoutUser(sel_user.attr("data-id"));
     }
     $('#users_list p:first').addClass("list_selected_element");
@@ -57,11 +57,15 @@ $("#useradd_btn").click(function() {
 $("#circuit").sortable({
     items: '.perso_circuit',
     placeholder: 'emplacement',
-    update: function(event, ui) {
+    update: function (event, ui) {
         creerFleches();
     },
-    over: function(e, ui) { sortableIn = 1; },
-    out: function(e, ui) { sortableIn = 0; },
+    over: function (e, ui) {
+        sortableIn = 1;
+    },
+    out: function (e, ui) {
+        sortableIn = 0;
+    },
     beforeStop: function (event, ui) {
         if (sortableIn == 0) {
             elem = ui.item;
@@ -71,14 +75,14 @@ $("#circuit").sortable({
             creerFleches();
         }
     }
- }).disableSelection();
+}).disableSelection();
 
 
-$("#circuitadd_btn").click(function() {
+$("#circuitadd_btn").click(function () {
     var sel_circuit = $('#circuits_list .list_selected_element');
     var ordre = sel_circuit.attr("data-ordre").split(",");
-    if(ordre.length > 0) {
-        $.each($("#circuit .perso_circuit"), function(k,v) {
+    if (ordre.length > 0) {
+        $.each($("#circuit .perso_circuit"), function (k, v) {
             var elem = $(this);
             $("<p/>").attr("data-id", elem.data("id")).text(elem.find(".nom_perso").text()).appendTo("#users_list");
             elem.remove();
@@ -94,22 +98,21 @@ $("#circuitadd_btn").click(function() {
     $("#circuit_modifier, #btn-group-supp").show();
 });
 
-
- // si on est dans un form on lui passe le circuit qd on walid
+// si on est dans un form on lui passe le circuit qd on walid
 var form_parent = $("#circuit").parents("form");
 
-if(form_parent.length > 0 ) {
-     form_parent.submit(function(e) {
-         e.preventDefault();
-         var ordre_circuit = new Array();
-         $("#circuit .perso_circuit").each(function(k,v) {
+if (form_parent.length > 0) {
+    form_parent.submit(function (e) {
+        e.preventDefault();
+        var ordre_circuit = new Array();
+        $("#circuit .perso_circuit").each(function (k, v) {
             ordre_circuit.push($(this).data("id"));
-         });
-         $('<input />')
-             .attr({ 'type': 'hidden', 'name': "circuit" })
-             .val(ordre_circuit)
-             .appendTo(form_parent);
-         this.submit();
-         return false;
-     });
+        });
+        $('<input />')
+            .attr({ 'type': 'hidden', 'name': "circuit" })
+            .val(ordre_circuit)
+            .appendTo(form_parent);
+        this.submit();
+        return false;
+    });
 }
