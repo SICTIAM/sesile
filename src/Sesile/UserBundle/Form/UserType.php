cefa626f6aad23c3338ccd59b53602f4bb7db3c5
Julien Mercier <j.mercier@sictiam.fr>
2014-01-15 18:30:57 +0100

    admin pas fini

--- src/Sesile/UserBundle/Form/UserType.php
+++ src/Sesile/UserBundle/Form/UserType.php
@@ -27,18 +27,18 @@ class UserType extends AbstractType
                 'second_options' => array('label' => 'form.password_confirmation'),
                 'invalid_message' => 'fos_user.password.mismatch',
             ))
-            ->add('Nom')
-            ->add('Prenom')
-            ->add('ville')
-            ->add('cp', 'text', array('label' => 'Code Postal'))
-            ->add('departement', 'text', array('label' => 'Département'))
-            ->add('pays')
-            ->add('role', 'text', array('label' => 'Rôle'))
+            ->add('Nom', 'text', array('required' => false,))
+            ->add('Prenom', 'text', array('required' => false,))
+            ->add('ville', 'text', array('required' => false,))
+            ->add('cp', 'text', array('label' => 'Code Postal', 'required' => false,))
+            ->add('departement', 'text', array('label' => 'Département', 'required' => false,))
+            ->add('pays', 'text', array('required' => false,))
+            ->add('role', 'text', array('label' => 'Rôle', 'required' => false,))
             ->add('enabled', null, array('label' => 'Activé'))
             ->add('file', 'file', array('label' => 'Avatar',
                 'data_class' => null,
                 'required' => false,
-            ));;
+            ));
 
     }
 
