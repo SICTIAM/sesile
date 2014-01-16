7150caffee21b4d4bcd1c76cac91855ac830c648
Christophe <c.dupont@sictiam.fr>
2014-01-16 14:47:39 +0100

    Ajout de la troisieme migration

--- src/Sesile/UserBundle/Entity/User.php
+++ src/Sesile/UserBundle/Entity/User.php
@@ -28,7 +28,7 @@ class User extends BaseUser
     /**
      * @var string
      *
-     * @ORM\Column(name="Nom", type="string", length=255)
+     * @ORM\Column(name="Nom", type="string", length=255, nullable=true)
      */
     protected $Nom;
 
