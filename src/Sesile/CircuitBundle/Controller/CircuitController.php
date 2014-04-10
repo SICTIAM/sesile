b02d047034e7827b34e00b189292bb9aff798426
regis <r.lasry@sictiam.fr>
2014-04-10 15:18:12 +0200

    Groupes dans la liste des circuits

--- src/Sesile/CircuitBundle/Controller/CircuitController.php
+++ src/Sesile/CircuitBundle/Controller/CircuitController.php
@@ -4,6 +4,7 @@ namespace Sesile\CircuitBundle\Controller;
 
 use Sesile\CircuitBundle\Entity\Circuit;
 use Sesile\UserBundle\Entity\Groupe;
+use Sesile\UserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -18,7 +19,7 @@ class CircuitController extends Controller
      * @Route("/new/", name="new_circuit", options={"expose"=true})
      * @Template()
      */
-    public function newAction()
+    public function newAction($forClasseur = true)
     {
         // recup la liste des users en base
         $em = $this->getDoctrine()->getManager();
@@ -28,8 +29,27 @@ class CircuitController extends Controller
 
         // recup la list des circuits
         // TODO recup uniquement pour le user connecté
+        $circuits = array();
         $em = $this->getDoctrine()->getManager();
-        $circuits = $em->getRepository('SesileCircuitBundle:Circuit')->findAll();
+        $circuits_du_user = $em->getRepository('SesileCircuitBundle:Circuit')->findByUser_id($this->getUser()->getId());
+
+        foreach ($circuits_du_user as $circuit) {
+            $circuits[] = array("id" => $circuit->getId(), "name" => $circuit->getName(), "ordre" => $circuit->getOrdre(), "groupe" => false);
+        }
+
+        if($forClasseur) {
+            $groupes_du_user = $em->getRepository('SesileUserBundle:UserGroupe')->findByUser($this->getUser());
+            foreach($groupes_du_user as $group) {
+                $circuits[] = array(
+                    "id" => $group->getGroupe()->getId(),
+                    "name" => $group->getGroupe()->getNom(),
+                    "ordre" => $this->getCircuitFromgroupForUser($this->getUser(), $group->getgroupe()),
+                    "groupe" => true
+                );
+            }
+        }
+
+
         return array('users' => $users, "circuits" => $circuits);
     }
 
@@ -39,15 +59,14 @@ class CircuitController extends Controller
      */
     public function indexAction()
     {
-        return $this->newAction();
+        return $this->newAction(false);
     }
 
     /**
      * @Route("/", name="create_circuit")
      * @Method("POST")
      */
-    public function createAction(Request $request)
-    {
+    public function createAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         $circuit = new Circuit();
         $circuit->setName($request->request->get('circuit_name'));
@@ -71,8 +90,7 @@ class CircuitController extends Controller
     /**
      * @Route("/liste", name="circuits_favoris", options={"expose"=true})
      */
-    public function listAction()
-    {
+    public function listAction() {
         $em = $this->getDoctrine()->getManager();
         $circuits_du_user = $em->getRepository('SesileCircuitBundle:Circuit')->findAll();
 
@@ -80,6 +98,7 @@ class CircuitController extends Controller
         foreach ($circuits_du_user as $circuit) {
             $circuits[] = array("id" => $circuit->getId(), "name" => $circuit->getName(), "ordre" => $circuit->getOrdre());
         }
+
 
         $response = new Response(json_encode($circuits));
         $response->headers->set('Content-Type', 'application/json');
@@ -107,19 +126,43 @@ class CircuitController extends Controller
     }
 
     /**
-     * Retourne le circuit associé à un groupe
+     * Retourne le circuit associé à un groupe pour un user donné
      * @param Groupe $group
+     * @param User $user
      * @return string les id user du circuit dans l'ordre
      */
-    private function getCircuitfromgroup(Groupe $group) {
+    private function getCircuitFromgroupForUser(User $user, Groupe $group) {
+        ini_set("memory_limit", 1024);
+
         $em = $this->getDoctrine()->getManager();
-        $repo = $em->getRepository('SesileCircuitBundle:UserGroupe');
+        $hierarchie = $em->getRepository('SesileUserBundle:UserGroupe')->findBy(array("groupe", $group), array("user", "DESC"));
 
-        $query = $repo->createQueryBuilder('p')
-            ->where('p.groupe > :groupe')
-            ->setParameter('groupe', $group)
-            ->getQuery();
+        $parent = false;
+        $ordre = array();
 
-        $products = $query->getResult();
+        $parent = $hierarchie->getParent();
+        $user_id = $hierarchie->getUser()->getId();
+
+        $ordre = $user_id.",";
+/*
+        while($parent !== 0) {
+            $userobj = $em->getRepository('SesileUserBundle:User')->findById($user_id);
+            $query = $repo->createQueryBuilder('p')
+                ->where('p.groupe = :groupe')
+                ->andWhere('p.user = :user')
+                ->setParameter('groupe', $group)
+                ->setParameter('user', $userobj)
+                ->getQuery();
+
+            $hierarchie = $query->getResult();
+            $hierarchie = $hierarchie[0];
+            $parent = $hierarchie->getParent();
+            $user_id = $hierarchie->getUser()->getId();
+            $ordre = $user_id.",";
+        }
+*/
+        var_dump($ordre);exit;
+
+        return $ordre;
     }
 }
