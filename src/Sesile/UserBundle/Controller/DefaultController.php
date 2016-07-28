d0f076eaa767599a44fb1a3df080b44897bc49a2
f.laussinot <f.laussinot@sictiam.fr>
2016-07-28 11:17:28 +0200

    Demande de CB : désactiver l envoie de mail lors de la création d utilisateur dans Sesile

--- src/Sesile/UserBundle/Controller/DefaultController.php
+++ src/Sesile/UserBundle/Controller/DefaultController.php
@@ -137,13 +137,13 @@ class DefaultController extends Controller
                     // fin de la modification des roles
 
                     //envoi d'un mail à l'utilisateur nouvellement créé
-                    $message = \Swift_Message::newInstance()
+                    /*$message = \Swift_Message::newInstance()
                         ->setContentType('text/html')
                         ->setSubject('Nouvel utilisateur')
                         ->setFrom("sesile@sictiam.fr")
                         ->setTo($entity->getUsername())
                         ->setBody('Bienvenue dans Sesile ' . $entity->getPrenom() . ' ' . $entity->getNom());
-                    $this->get('mailer')->send($message);
+                    $this->get('mailer')->send($message);*/
                 } else {
                     $this->get('session')->getFlashBag()->add(
                         'error',
