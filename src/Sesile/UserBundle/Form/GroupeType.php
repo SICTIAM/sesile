ef0975c6c66e879b61eaff98cf9220fa941d5b7f
ali boulajine <abdelaliboulajine@gmail.com>
2018-04-10 11:38:19 +0200

    [Evo] fix  #2174 - Remove useless code
    - Remove useless import 
    - Remove duplication of serializer group name

--- src/Sesile/UserBundle/Form/GroupeType.php
+++ src/Sesile/UserBundle/Form/GroupeType.php
@@ -2,7 +2,6 @@
 
 namespace Sesile\UserBundle\Form;
 
-use function Sodium\add;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
