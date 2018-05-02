78d7a3fdc2829d854d4195ca26bd56428e474edc
Panagiotis Gergos <panosger@gmail.com>
2018-05-02 10:02:48 +0200

    add main domain control endpoint

--- src/Sesile/MainBundle/Controller/DefaultController.php
+++ src/Sesile/MainBundle/Controller/DefaultController.php
@@ -7,6 +7,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Rest\Route("", options = { "expose" = true })
@@ -21,6 +23,24 @@ class DefaultController extends Controller
     public function appAction()
     {
         return $this->render('app.html.twig');
+    }
+    /**
+     * Return true if on main domain name (from config.yml parameters.domain)
+     * returns false if on subdomain
+     *
+     * @Route("/domain/control")
+     * @Template()
+     */
+    public function mainDomainAction(Request $request)
+    {
+//        $url = parse_url($request->server->get('HTTP_HOST'));
+        $url = $request->server->get('HTTP_HOST');
+        $subdomain = str_replace('.'. $this->getParameter('domain'), '' , $url);
+        if ($subdomain == $url) {
+            return new JsonResponse(['main' => true, 'mainDomain' => $this->getParameter('domain'), 'currentDomain' => $request->server->get('HTTP_HOST')], Response::HTTP_OK);
+        }
+
+        return new JsonResponse(['main' => false, 'mainDomain' => $this->getParameter('domain'), 'currentDomain' => $request->server->get('HTTP_HOST')], Response::HTTP_OK);
     }
 
     /**
