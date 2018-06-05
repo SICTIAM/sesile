fc4feaee033d615c3e66c32e27fb5b133c3f4861
Panagiotis Gergos <panosger@gmail.com>
2018-06-05 10:15:00 +0200

    minor fix on doctrine migration script

--- app/DoctrineMigrations/Version20180501141159.php
+++ app/DoctrineMigrations/Version20180501141159.php
@@ -29,7 +29,7 @@ class Version20180501141159 extends AbstractMigration
         $this->addSql('UPDATE Classeur c SET c.collectivite_id = (SELECT u.collectivite FROM  User u WHERE u.id=c.user) WHERE c.collectivite_id IS NULL;');
 
         $this->addSql('CREATE TABLE Ref_Collectivite_User (user_id INT NOT NULL, collectivite_id INT NOT NULL, INDEX IDX_762C809A76ED395 (user_id), INDEX IDX_762C809A7991F51 (collectivite_id), PRIMARY KEY(user_id, collectivite_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('INSERT INTO Ref_Collectivite_User (user_id, collectivite_id) (SELECT u.id as user_id, u.collectivite as collectivite_id FROM User u)');
+        $this->addSql('INSERT INTO Ref_Collectivite_User (user_id, collectivite_id) (SELECT u.id as user_id, u.collectivite as collectivite_id FROM User u where u.collectivite is NOT NULL)');
         $this->addSql('ALTER TABLE Ref_Collectivite_User ADD CONSTRAINT FK_762C809A76ED395 FOREIGN KEY (user_id) REFERENCES User (id)');
         $this->addSql('ALTER TABLE Ref_Collectivite_User ADD CONSTRAINT FK_762C809A7991F51 FOREIGN KEY (collectivite_id) REFERENCES Collectivite (id)');
 
