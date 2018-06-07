c000329d35720fb542905c767576c642c74d846d
Panagiotis Gergos <panosger@gmail.com>
2018-06-07 16:57:49 +0200

    [EVO] ref #4184 Create new legacy collectivity manager and service + tests

--- src/Sesile/MigrationBundle/Tests/Manager/LegacyCollectivityManagerTest.php
+++ src/Sesile/MigrationBundle/Tests/Manager/LegacyCollectivityManagerTest.php
@@ -4,7 +4,52 @@
 namespace Sesile\MigrationBundle\Tests\Manager;
 
 
-class LegacyCollectivityManagerTest
-{
+use Doctrine\DBAL\Connection;
+use Psr\Log\LoggerInterface;
+use Sesile\MainBundle\Domain\Message;
+use Sesile\MigrationBundle\Manager\LegacyCollectivityManager;
+use Sesile\MigrationBundle\Service\LegacyCollectivityService;
+use Sesile\MigrationBundle\Tests\LegacyWebTestCase;
 
+class LegacyCollectivityManagerTest extends LegacyWebTestCase
+{
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+    /**
+     * @var LegacyCollectivityManager
+     */
+    protected $legacyCollectivityManager;
+    /**
+     * @var LegacyCollectivityService
+     */
+    protected $service;
+
+    public function setUp()
+    {
+        $this->service = $repository = $this->getMockBuilder(LegacyCollectivityService::class)
+            ->disableOriginalConstructor()
+            ->setMethods(
+                ['fetchData', 'getLegacyCollectivityList']
+            )->getMock();
+        $this->logger = $this->createMock(LoggerInterface::class);
+        $this->legacyCollectivityManager = new LegacyCollectivityManager($this->service, $this->logger);
+    }
+
+    public function testGetLegacyCollectivityList()
+    {
+        $this->service->expects(self::once())
+            ->method('fetchData')
+            ->willReturn(
+                [
+                    ['id' => '1', 'name' => 'Sictiam', 'domain' => 'sictiam'],
+                    ['id' => '2', 'name' => 'Département 06', 'domain' => 'departement06'],
+                ]
+            );
+        $result = $this->legacyCollectivityManager->getLegacyCollectivityList();
+        self::assertInstanceOf(Message::class, $result);
+        self::assertTrue($result->isSuccess());
+        self::assertCount(2, $result->getData());
+    }
 }
