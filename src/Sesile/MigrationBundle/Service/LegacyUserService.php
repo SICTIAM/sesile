5e73e6519ad6005caf2196b37585331df34ab695
Panagiotis Gergos <panosger@gmail.com>
2018-06-11 16:17:08 +0200

    [EVO] ref #3928 fetch legacy user roles create new method fixtures and tests

--- src/Sesile/MigrationBundle/Service/LegacyUserService.php
+++ src/Sesile/MigrationBundle/Service/LegacyUserService.php
@@ -20,4 +20,16 @@ class LegacyUserService extends BaseLegacyService
 
         return $this->fetchAllData($this->prepareQuery($query, ['collectivityId' => $collectivityId]));
     }
+
+    /**
+     * @param $userEmail
+     * @return array
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function getLegacyUserRoles($userEmail)
+    {
+        $query = "SELECT ur.userRoles FROM UserRole ur JOIN User u on ur.user = u.id WHERE u.email = :userEmail";
+
+        return $this->fetchAllData($this->prepareQuery($query, ['userEmail' => $userEmail]), \PDO::FETCH_COLUMN);
+    }
 }
--- src/Sesile/MigrationBundle/Tests/Service/LegacyUserServiceTest.php
+++ src/Sesile/MigrationBundle/Tests/Service/LegacyUserServiceTest.php
@@ -4,12 +4,13 @@
 namespace Sesile\MigrationBundle\Tests\Service;
 
 
+use Sesile\MigrationBundle\Service\LegacyUserService;
 use Sesile\MigrationBundle\Tests\LegacyWebTestCase;
 
 class LegacyUserServiceTest extends LegacyWebTestCase
 {
     /**
-     * @var LegacyCollectivityService
+     * @var LegacyUserService
      */
     protected $service;
 
@@ -54,6 +55,31 @@ class LegacyUserServiceTest extends LegacyWebTestCase
         self::assertArrayHasKey('pathSignature', $result[0]);
         self::assertArrayHasKey('qualite', $result[0]);
         self::assertArrayHasKey('sesile_version', $result[0]);
+    }
+
+    public function testGetLegacyUsersShouldReturnEmptyArrayIfNotFound()
+    {
+        $result = $this->service->getLegacyUsersByCollectivity('uknown');
+        self::assertCount(0, $result);
+    }
+
+    public function testGetUserRoles()
+    {
+        $userEmail = 'legacy@sictiam.fr';
+        $result = $this->service->getLegacyUserRoles($userEmail);
+        self::assertCount(2, $result);
+        self::assertEquals('Directeur Général', $result[0]);
+        self::assertEquals('Agent administratif', $result[1]);
+        $user2Email = 'user@sictiam.fr';
+        $result = $this->service->getLegacyUserRoles($user2Email);
+        self::assertCount(1, $result);
+        self::assertEquals('MAIRE', $result[0]);
+    }
+    public function testGetUserRolesShouldReturnEmptyArrayIfNothingFound()
+    {
+        $userEmail = 'uknown@sictiam.fr';
+        $result = $this->service->getLegacyUserRoles($userEmail);
+        self::assertCount(0, $result);
     }
 
 }
