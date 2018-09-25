c9dc4adb767147b83078dba46ae7aa7f590c36d4
Andrea Tombarel <andrea.tombarel@epitech.eu>
2018-09-25 17:01:32 +0200

    [Evo] Redesign groupes list
    - all in table
    - remove unnecessary labels
    - dropdown list for user

---
+++ app/Resources/assets/js/_components/ButtonDropdown.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { func, number, string } from 'prop-types'
+import { translate } from 'react-i18next'
+
+import { Button } from './Form'
+import { GridX, Cell } from './UI'
+import {DisplayLongText} from "../_utils/Utils";
+
+class ButtonPopup extends Component {
+    static contextTypes = {
+        t: func
+    }
+    static defaultProps = {
+        id: null,
+        content: '',
+        onConfirm: null,
+    }
+    state = {
+        isOpen: ''
+    }
+    componentDidMount = () => $(`#container-${this.props.dataToggle}`).foundation()
+    componentWillUnmount = () => $(`#${this.props.dataToggle}`).foundation('_destroy')
+    onConfirm() {
+        this.props.onConfirm(this.props.id)
+        $(`#${this.props.dataToggle}`).foundation('close')
+    }
+    render() {
+        const { t } = this.context
+        return (
+            <div id={`container-${this.props.dataToggle}`} className={this.props.className}>
+                <button data-toggle={this.props.dataToggle}>
+                    <DisplayLongText
+                        text={this.props.content.map(user => `${user._prenom} ${user._nom}`).join(' | ')}
+                        maxSize={40}/>
+                </button>
+                <div
+                    id={this.props.dataToggle}
+                    data-position="bottom"
+                    data-alignment="center"
+                    className={`dropdown-pane ${this.state.isOpen} dropdown-confirm-delete text-center`}
+                    data-dropdown
+                    data-auto-focus="true">
+                    <GridX className="grid-margin-y">
+                        <Cell className="medium-12 text-bold text-center">
+                            <span>{this.props.content.map(user =>
+                                <ul style={{marginLeft:"0"}}>{user._prenom} {user._nom}</ul>)}</span>
+                        </Cell>
+                    </GridX>
+                </div>
+            </div>
+        )
+    }
+}
+
+ButtonPopup.protoTypes = {
+    id: number.isRequired,
+    contenttext: string.isRequired,
+    content: string.isRequired,
+    onConfirm: func.isRequired,
+    dataToggle: string,
+    className: string
+}
+
+export default translate(['sesile'])(ButtonPopup)
--- app/Resources/assets/js/admin/Groups.jsx
+++ app/Resources/assets/js/admin/Groups.jsx
@@ -12,6 +12,8 @@ import SelectCollectivite from '../_components/SelectCollectivite'
 
 import { escapedValue } from '../_utils/Search'
 import { handleErrors, DisplayLongText } from "../_utils/Utils"
+import History from "../_utils/History";
+import ButtonDropdown from "../_components/ButtonDropdown";
 
 class Groups extends Component {
     static contextTypes = {
@@ -38,19 +40,19 @@ class Groups extends Component {
             method: 'DELETE',
             credentials: 'same-origin'
         })
-        .then(handleErrors)
-        .then(response => response.json())
-        .then(groups => {
-            _addNotification(
-                basicNotification(
-                    'success',
-                    t('admin.group.success_delete')))
-            this.setState({groups, filteredGroups: groups})})
-        .catch(() =>
-            _addNotification(
-                basicNotification(
-                    'error',
-                    t('admin.group.error_delete'))))
+            .then(handleErrors)
+            .then(response => response.json())
+            .then(groups => {
+                _addNotification(
+                    basicNotification(
+                        'success',
+                        t('admin.group.success_delete')))
+                this.setState({groups, filteredGroups: groups})})
+            .catch(() =>
+                _addNotification(
+                    basicNotification(
+                        'error',
+                        t('admin.group.error_delete'))))
     }
     getListGroupe(collectiviteId) {
         fetch(
@@ -58,8 +60,8 @@ class Groups extends Component {
                 'sesile_user_userpackapi_getbycollectivite',
                 {collectiviteId}),
             {credentials: 'same-origin'})
-        .then(response => response.json())
-        .then(json => this.setState({groups: json, filteredGroups: json}))
+            .then(response => response.json())
+            .then(json => this.setState({groups: json, filteredGroups: json}))
     }
     handleChangeCollectivite = (collectiviteId) => {
         this.setState({collectiviteId, userName: '', groupName: ''})
@@ -88,43 +90,57 @@ class Groups extends Component {
                 handleClickDelete={this.handleClickDelete} />)
         return (
             <AdminPage
-                title={t('admin.title', {name: t('admin.group.name')})}
-                subtitle={t('admin.subtitle')}>
+                title={t('admin.group.name')}>
                 <AdminContainer>
-                    <Cell className="medium-6">
-                        <GridX className="grid-padding-x align-center-middle">
-                            <Input
-                                className="cell medium-auto"
-                                labelText={t('admin.group.which_group')}
-                                value={this.state.groupName}
-                                onChange={this.handleSearchByGroupName}
-                                placeholder={t('common.search_by_name')}
-                                type="text"/>
-                            <Input
-                                className="cell medium-auto"
-                                labelText={t('admin.user.which_user')}
-                                value={this.state.userName}
-                                onChange={this.handleSearchByUserName}
-                                placeholder={t('common.search_by_name')}
-                                type="text"/>
+                    <div className="grid-x grid-padding-x panel align-center-middle" style={{width:"74em", marginTop:"1em"}}>
+                        <div className="grid-x grid-padding-x panel align-center-middle" style={{display:"flex", marginBottom:"0em", marginTop:"10px", width:"77%"}}>
+                            <div className="" style={{marginTop:"10px",marginLeft:"1%",width:"14em",marginRight:"10px"}}>
+                                <Input
+                                    className=""
+                                    value={this.state.groupName}
+                                    onChange={this.handleSearchByGroupName}
+                                    placeholder={t('common.search_by_groupname')}
+                                    type="text"/>
+                            </div>
+                            <div className="" style={{marginTop:"10px",marginLeft:"1%",width:"14em",marginRight:"10px"}}>
+                                <Input
+                                    className=""
+                                    value={this.state.userName}
+                                    onChange={this.handleSearchByUserName}
+                                    placeholder={t('common.search_by_name')}
+                                    type="text"/>
+                            </div>
                             {this.state.isSuperAdmin &&
-                                <Cell className="medium-auto">
-                                    <SelectCollectivite
-                                        currentCollectiviteId={this.state.collectiviteId}
-                                        handleChange={this.handleChangeCollectivite} />
-                                </Cell>}
-                        </GridX>
-                    </Cell>
-                    <AdminList
-                        title={t('admin.group.groups_list')}
-                        listLength={listFilteredGroups.length}
-                        labelButton={t('admin.group.add_group')}
-                        addLink={`/admin/${this.state.collectiviteId}/groupe`}
-                        headTitles={[t('common.label.name'), t('admin.associated_users'), t('common.label.actions')]}
-                        headGrid={['medium-3', 'medium-auto', 'medium-2']}
-                        emptyListMessage={t('common.no_results', {name: t('admin.group.name')})}>
-                        {listFilteredGroups}
-                    </AdminList>
+                            <div className="" style={{marginTop:"10px",marginLeft:"1%",width:"14em",marginRight:"10px"}}>
+                                <SelectCollectivite
+                                    currentCollectiviteId={this.state.collectiviteId}
+                                    handleChange={this.handleChangeCollectivite} />
+                            </div>}
+                            <div className="text-right" style={{marginLeft:"10px",marginRight:"1%"}}>
+                                <button className="button" onClick={() => History.push(`/admin/${this.state.collectiviteId}/groupe`)} style={{backgroundColor:"transparent", border:"1px solid rgb(204, 0, 102)", color:"rgb(204, 0, 102)"}}>{t('admin.group.add_group')}</button>
+                            </div>
+                        </div>
+                        <table style={{margin:"10px", borderRadius:"6px"}}>
+                            <thead>
+                            <tr style={{backgroundColor:"#CC0066", color:"white"}}>
+                                <td width="240px" className="text-bold">{ t('common.label.name') }</td>
+                                <td width="240px" className="text-bold">{ t('admin.associated_users') }</td>
+                                <td width="50px" className="text-bold">{  t('common.label.actions') }</td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {listFilteredGroups.length > 0 ?
+                                listFilteredGroups :
+                                <tr>
+                                    <td>
+                                        <span style={{textAlign:"center"}}>{this.props.message}</span>
+                                    </td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>}
+                            </tbody>
+                        </table>
+                    </div>
                 </AdminContainer>
             </AdminPage>
         )
@@ -138,34 +154,24 @@ Groups.PropTypes = {
 export default translate(['sesile'])(Groups)
 
 const RowGroup = ({group, collectiviteId, handleClickDelete}, {t}) =>
-    <AdminListRow>
-        <Cell className="medium-3">
-            <DisplayLongText text={group.nom} maxSize={30}/>
-        </Cell>
-        <Cell className="medium-auto">
-            <DisplayLongText
-                text={group.users.map(user => user._nom).join(' | ')}
-                title={group.users.map(user => `${user._prenom} ${user._nom}`).join('\n')}
-                maxSize={100}/>
-        </Cell>
-        <Cell className="medium-2">
-            <GridX>
-                <Cell className="medium-auto">
-                    <Link
-                        to={`/admin/${collectiviteId}/groupe/${group.id}`}
-                        className="fa fa-pencil icon-action"
-                        title={t('common.button.edit')}/>
-                </Cell>
-                <Cell className="medium-auto">
-                    <ButtonConfirmDelete
-                        id={group.id}
-                        dataToggle={`delete-confirmation-update-${group.id}`}
-                        onConfirm={handleClickDelete}
-                        content={t('common.confirm_deletion_item')}/>
-                </Cell>
-            </GridX>
-        </Cell>
-    </AdminListRow>
+    <tr>
+        <td onClick={() => History.push(`/admin/${collectiviteId}/groupe/${group.id}`)} style={{cursor:"Pointer"}}>
+            {group.nom}
+        </td>
+        <td>
+            <ButtonDropdown
+                id={group.id}
+                dataToggle={`user-list-${group.id}`}
+                content={group.users}/>
+        </td>
+        <td>
+            <ButtonConfirmDelete
+                id={group.id}
+                dataToggle={`delete-confirmation-update-${group.id}`}
+                onConfirm={handleClickDelete}
+                content={t('common.confirm_deletion_item')}/>
+        </td>
+    </tr>
 
 RowGroup.propTypes = {
     group: object.isRequired,
