2f2cd04680bc3fd9c17338face7db59b0eb88a47
Andrea Tombarel <andrea.tombarel@epitech.eu>
2018-09-28 14:51:01 +0200

    [Fix] Ref #4823 - Graphic Upgrade Help
    - fix too long line
    - fix config file

--- app/Resources/assets/js/documentation/DocumentationRow.jsx
+++ app/Resources/assets/js/documentation/DocumentationRow.jsx
@@ -3,7 +3,7 @@ import { func, object, string } from 'prop-types'
 import { translate } from 'react-i18next'
 import { Link } from 'react-router-dom'
 import Moment from 'moment'
-import {DisplayLongText} from "../_utils/Utils";
+import {DisplayLongText} from "../_utils/Utils"
 import {Cell} from "../_components/UI";
 
 class DocumentationRow extends Component {
@@ -22,7 +22,7 @@ class DocumentationRow extends Component {
         const { t } = this.context
 
         return (
-            <div onClick={() => window.open(Routing.generate(download_route, {id: documentation.id}), "_blank")} style={{padding:"0.5em", cursor:"pointer", width: "360px", display: "inline-block", marginRight: "1em", marginBottom: "1em", boxShadow: "rgba(34, 36, 38, 0.15) 0px 1px 2px 0px", borderRadius: "0.285714rem", border: "1px solid rgba(34, 36, 38, 0.15)"}}>
+            <div className="HelpBlock" onClick={() => window.open(Routing.generate(download_route, {id: documentation.id}), "_blank")}>
                 <div className="text-bold"><DisplayLongText text={documentation.description} maxSize={30}/></div>
                 <div style={{display:"flex", marginTop:"0.5em"}}>
                     <div className="align-left" style={{width:"92%"}}>{ Moment(documentation.date).format('LL') }</div>
