646817fb8558fa841d1fa44f58945e4a063834ca
Andrea Tombarel <andrea.tombarel@epitech.eu>
2018-10-01 15:43:50 +0200

    [Evo] Ref #4838 - improve type page

--- app/Resources/assets/js/admin/Type.jsx
+++ app/Resources/assets/js/admin/Type.jsx
@@ -45,21 +45,21 @@ class Type extends Component {
                 }),
                 credentials: 'same-origin'
             })
-            .then(handleErrors)
-            .then(response => response.json())
-            .then(() => {
-                this.setState({edit: false})
-                this.context._addNotification(
-                    basicNotification(
-                        'success',
-                        this.context.t('admin.type.success_save')))
-                this.props.fetchTypes(this.props.collectiviteId)
-            })
-            .catch(() =>
-                this.context._addNotification(
-                    basicNotification(
-                        'error',
-                        this.context.t('admin.type.error_save'))))
+                .then(handleErrors)
+                .then(response => response.json())
+                .then(() => {
+                    this.setState({edit: false})
+                    this.context._addNotification(
+                        basicNotification(
+                            'success',
+                            this.context.t('admin.type.success_save')))
+                    this.props.fetchTypes(this.props.collectiviteId)
+                })
+                .catch(() =>
+                    this.context._addNotification(
+                        basicNotification(
+                            'error',
+                            this.context.t('admin.type.error_save'))))
         }
     }
     handleClickRemove = () => {
@@ -67,20 +67,20 @@ class Type extends Component {
             method: 'delete',
             credentials: 'same-origin'
         })
-        .then(handleErrors)
-        .then(response => response.json())
-        .then(() => {
-            this.context._addNotification(
-                basicNotification(
-                    'success',
-                    this.context.t('admin.type.success_delete')))
-            this.props.fetchTypes(this.props.collectiviteId)
-        })
-        .catch(() =>
-            this.context._addNotification(
-                basicNotification(
-                    'error',
-                    this.context.t('admin.type.error_delete'))))
+            .then(handleErrors)
+            .then(response => response.json())
+            .then(() => {
+                this.context._addNotification(
+                    basicNotification(
+                        'success',
+                        this.context.t('admin.type.success_delete')))
+                this.props.fetchTypes(this.props.collectiviteId)
+            })
+            .catch(() =>
+                this.context._addNotification(
+                    basicNotification(
+                        'error',
+                        this.context.t('admin.type.error_delete'))))
     }
     handleClickEdit = () => this.setState(prevState => {edit: prevState.edit = !prevState.edit})
     handleClickCancel = () => {
@@ -91,53 +91,61 @@ class Type extends Component {
         const { t } = this.context
         const { type } = this.state
         return (
-            <AdminListRow>
-                <Cell className="medium-auto">
-                    <GridX>
-                        <Cell className="medium-7">
-                            {this.state.edit ?
-                                <InputValidation
-                                    id="nom"
-                                    type="text"
-                                    value={type.nom}
-                                    validationRule={this.validationRules.nom}
-                                    onChange={this.handleChangeName}
-                                    autoFocus={this.state.edit}
-                                    placeholder={t('common.type_a_name')}/> :
-                                type.nom}
-                        </Cell>
-                    </GridX>
-                </Cell>
-                <Cell className="medium-2">
-                    <GridX>
-                        {this.state.edit &&
-                            <Cell className="medium-auto">
+            <tr>
+                <td>
+                    {this.state.edit ?
+                        <div style={{width:"50%"}}>
+                            <InputValidation
+                                id="nom"
+                                type="text"
+                                value={type.nom}
+                                validationRule={this.validationRules.nom}
+                                onChange={this.handleChangeName}
+                                autoFocus={this.state.edit}
+                                placeholder={t('common.type_a_name')}/>
+                        </div> :
+                        type.nom}
+                </td>
+                <td>
+                    {type.supprimable ?
+                        <div style={{display:"inline-flex"}}>
+                            {this.state.edit &&
+                            <div className="medium-auto" style={{marginLeft: "15px", marginTop: "10px"}}>
                                 <span
                                     onClick={() => this.handleClickCancel()}
                                     className="fa fa-undo icon-action"
                                     title={t('common.button.cancel')}/>
-                            </Cell>}
-                        <Cell className="medium-auto">
-                            {this.state.edit ?
-                                <span
-                                    onClick={() => this.handleClickUpdate()}
-                                    className="fa fa-check icon-action"
-                                    title={t('common.button.save')}/> :
-                                <span
-                                    onClick={() => this.handleClickEdit()}
-                                    className="fa fa-pencil icon-action"
-                                    title={t('common.button.edit')}/>}
-                        </Cell>
-                        <Cell className="medium-auto">
-                            <ButtonConfirmDelete
-                                id={this.state.type.id}
-                                dataToggle={`delete-confirmation-update-${this.props.type.id}`}
-                                onConfirm={() => this.handleClickRemove()}
-                                content={t('common.confirm_deletion_item')}/>
-                        </Cell>
-                    </GridX>
-                </Cell>
-            </AdminListRow>
+                            </div>}
+                            <div className="medium-auto" style={{marginLeft: "15px", marginTop: "10px"}}>
+                                {this.state.edit ?
+                                    <span
+                                        onClick={() => this.handleClickUpdate()}
+                                        className="fa fa-check icon-action"
+                                        title={t('common.button.save')}/> :
+                                    <span
+                                        onClick={() => this.handleClickEdit()}
+                                        className="fa fa-pencil icon-action"
+                                        title={t('common.button.edit')}/>}
+                            </div>
+                            <div className="medium-auto" style={{marginLeft: "15px", marginTop: "10px"}}>
+                                <ButtonConfirmDelete
+                                    id={this.state.type.id}
+                                    dataToggle={`delete-confirmation-update-${this.props.type.id}`}
+                                    onConfirm={() => this.handleClickRemove()}
+                                    content={t('common.confirm_deletion_item')}/>
+                            </div>
+                        </div> :
+                        <div style={{display:"inline-flex"}}>
+                            <div className="medium-auto" style={{marginLeft:"15px", marginTop:"10px"}}>
+                                <span className="fa fa-pencil icon-action" style={{color: "rgba(138, 138, 137, 0.58)", cursor:"not-allowed"}}/>
+                            </div>
+                            <div className="medium-auto" style={{marginLeft:"15px", marginTop:"10px"}}>
+                                <span className="fa fa-trash medium icon-action" style={{color: "rgba(138, 138, 137, 0.58)", cursor:"not-allowed"}}/>
+                            </div>
+                        </div>
+                    }
+                </td>
+            </tr>
         )
     }
 }
