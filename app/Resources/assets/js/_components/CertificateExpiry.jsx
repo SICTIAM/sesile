86094ba9bedff77b6de406dd9903d3d244f5a540
Andrea Tombarel <andrea.tombarel@epitech.eu>
2018-10-15 08:50:58 +0200

    [Evo] Ref #4681 - Dashboard remaining days color
    - red if certificate expire in less than 60 days

--- app/Resources/assets/js/_components/CertificateExpiry.jsx
+++ app/Resources/assets/js/_components/CertificateExpiry.jsx
@@ -4,17 +4,29 @@ import {Link} from "react-router-dom";
 const CertificateValidity = ({ certificate, certificateRemainingDays, CertifRemain, NoCertif }) =>
     <div className="cell small-6 medium-6 large-6">
         {(certificate && certificateRemainingDays) ?
-            <Link
-                className="text-bold"
-                style={{textDecoration:"underline"}}
-                to="/utilisateur/certificat-electronique">
+            certificateRemainingDays > 60 ?
+                <Link
+                    className="text-bold"
+                    style={{textDecoration: "underline"}}
+                    to="/utilisateur/certificat-electronique">
                     <span style={{margin: '5px'}}>
                         {CertifRemain}
                     </span>
-            </Link> :
+                </Link>
+                :
+                <Link
+                    className="text-bold text-alert"
+                    style={{textDecoration: "underline"}}
+                    to="/utilisateur/certificat-electronique">
+                    <span style={{margin: '5px'}}>
+                        {CertifRemain}
+                    </span>
+                </Link>
+            :
             <span style={{margin: '5px'}}>
                 {NoCertif}
-            </span>}
+            </span>
+        }
     </div>
 
 export { CertificateValidity }
