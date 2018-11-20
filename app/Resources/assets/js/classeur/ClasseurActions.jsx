5d7500e512a795cf752b49eb23ed2444c05c0d7a
Andrea Tombarel <andrea.tombarel@epitech.eu>
2018-11-20 14:09:28 +0100

    [Evo] Ref#5161 - Classeurs comments scrollable

--- app/Resources/assets/js/classeur/ClasseurActions.jsx
+++ app/Resources/assets/js/classeur/ClasseurActions.jsx
@@ -42,7 +42,7 @@ class ClasseurActions extends Component {
                                 className="cell medium-10 text-right"
                                 onClick={submitComment}/>
                     </div>
-
+                    <div style={{height:"22em", overflow:"auto"}}>
                     { actions && actions.map((action) =>
                         <div key={action.id}>
                             <hr style={{height: '0.2rem', margin: '1rem auto'}}/>
@@ -70,6 +70,7 @@ class ClasseurActions extends Component {
                             </div>
                         </div>
                     )}
+                    </div>
                 </div>
             </div>
 
